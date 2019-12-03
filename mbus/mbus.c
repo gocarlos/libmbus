67f94c906086b2e52d2fa4b31d1c066574ccf872
Carlos Gomes Martinho <carlos.gomes_martinho@siemens.com>
2019-12-03 16:41:11 +0100

    build: add cmake

--- mbus/mbus.c
+++ mbus/mbus.c
@@ -8,16 +8,15 @@
 //
 //------------------------------------------------------------------------------
 
+#include "config.h"
 #include "mbus-protocol.h"
-#include "../config.h"
 
 //
 //
 //
-int mbus_init() {return 0;}
+int mbus_init() { return 0; }
 
 ///
 /// Return current version of the library
 ///
-const char*
-mbus_get_current_version() {return VERSION;}
+const char* mbus_get_current_version() { return VERSION; }
